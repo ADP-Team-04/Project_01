d60eda3108ba618ecae74511ddce94830220485f
Vutsey <vutv579@gmail.com>
2023-03-25 18:57:32 +0700

    UpdateCode_25/03

--- assets/js/minus.js
+++ assets/js/minus.js
@@ -1,11 +1,10 @@
 var x,y,minus;
 function clickResult(){  
-    window.location.href = 'resultMulti.html'
+    window.location.href = 'resultMinus.html'
     x = Number(eval(document.getElementById("num1").value));
     y = Number(eval(document.getElementById("num2").value));
     minus = x-y;
-    // localStorage.setItem("in ra man hinh",minus);
-    document.getElementById("abc").innerHTML = minus;
+    localStorage.setItem("Input",minus);
 }
 function deleteEle(){
     document.getElementById("num1".value);
