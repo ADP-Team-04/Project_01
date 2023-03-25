436844ab632567bc7916933bfb0246426dd6a9d1
Vutsey <vutv579@gmail.com>
2023-03-25 18:45:29 +0700

    updateCode_25/03/2023

--- assets/js/minus.js
+++ assets/js/minus.js
@@ -1,7 +1,13 @@
-let x,y,minus;
+var x,y,minus;
 function clickResult(){  
+    window.location.href = 'resultMulti.html'
     x = Number(eval(document.getElementById("num1").value));
     y = Number(eval(document.getElementById("num2").value));
     minus = x-y;
-    localStorage.setItem("Minus",minus);
+    // localStorage.setItem("in ra man hinh",minus);
+    document.getElementById("abc").innerHTML = minus;
+}
+function deleteEle(){
+    document.getElementById("num1".value);
+    document.getElementById("num1".value);
 }
