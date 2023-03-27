5a1c03b353a42ee17e68cfd2eb85111a10085e0f
suixmi <ptnanh125@gmail.com>
2023-03-28 01:21:16 +0700

    update 27/03/2023

--- assets/js/plusJs.js
+++ assets/js/plusJs.js
@@ -1,11 +1,55 @@
-var a,b;
-var result;
+var num1,num2;
+var result1;
+var result2;
+var kqua;
+
 function clickResult() {
-    a = Number(eval(document.getElementById("num1").value));
-    b = Number(eval(document.getElementById("num2").value));
-    result = String(a+b);
+    document.getElementById("alert-error1").innerHTML = '&emsp;';
+    document.getElementById("alert-error2").innerHTML = '&emsp;';
+    nofalse = true;
+    num1= document.getElementById("num1").value.trim();
+    num2= document.getElementById('num2').value.trim();
+    console.log(num1);
+    console.log(num2);
+    if(num1 === "" ) {
+        nofalse = false;
+        document.getElementById("alert-error1").innerHTML = '*Please do not leave it blank';
+    }
+    if(num2 === "" ) {
+        nofalse = false;
+        document.getElementById("alert-error2").innerHTML = '*Please do not leave it blank';
+    }
+    try {
+        result1 = new Function('return ' + num1)();
+     } catch (err) {
+        document.getElementById("alert-error1").innerHTML = '*please enter again';
+        nofalse = false;
+        try {
+            result2 = new Function('return ' + num2)();
+         } catch (err) {
+            document.getElementById("alert-error2").innerHTML = "*please enter again";
+         }
+     }
+     try {
+        result2 = new Function('return ' + num2)();
+     } catch (err) {
+        document.getElementById("alert-error2").innerHTML = '*please enter again';
+        nofalse = false;
+     }
+    if(nofalse) {
+        window.location.href = 'resultplus.html';
+    }
+    kqunum1= result1*result2;
+    console.log(kqua);
+    localStorage.setItem("in", kqua);
 }
 function deleteEle() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
+}
+function clickreturn(){
+    window.location.href = 'index.html';
+}
+function clickContinue(){
+    window.location.href = 'plus.html';
 }
