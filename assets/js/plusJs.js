1f93ce959fa7c4859a850609aead25ded16d0b2d
suixmi <ptnanh125@gmail.com>
2023-03-29 01:16:25 +0700

    update code 28/03/2023

--- assets/js/plusJs.js
+++ assets/js/plusJs.js
@@ -2,7 +2,10 @@ var num1,num2;
 var result1;
 var result2;
 var kqua;
-
+var times = 0;
+var input1 = false;
+var input2 = false;
+var nofalse = true;
 function clickResult() {
     document.getElementById("alert-error1").innerHTML = '&emsp;';
     document.getElementById("alert-error2").innerHTML = '&emsp;';
@@ -39,7 +42,7 @@ function clickResult() {
     if(nofalse) {
         window.location.href = 'resultplus.html';
     }
-    kqunum1= result1*result2;
+    kqua= result1+result2;
     console.log(kqua);
     localStorage.setItem("in", kqua);
 }
@@ -52,4 +55,54 @@ function clickreturn(){
 }
 function clickContinue(){
     window.location.href = 'plus.html';
+}
+function clickCopy(){
+    let text = document.getElementById|('result-out').value;
+    navigator.clipboard.writeText(text);
+    alert('Result is copied')
+}
+function show_keyboard() {  
+    times++;
+    var board =document.getElementById("table-board-box");
+    var but1 = document.getElementById("return-click");
+    var but2 = document.getElementById("result-click");
+    var but3 = document.getElementById("delete-click");
+    var plusbox = document.getElementById("idToAdd");
+    var keyboard= document.getElementById("keyboard")
+    if(times%2 != 0) {   
+        board.style.cssText = "display: block; animation: fadeOut 0.65s forwards;";
+        but1.style.cssText = "visibility: hidden;";
+        but2.style.cssText = "visibility: hidden;";
+        but3.style.cssText = "visibility: hidden;";
+        plusbox.style.cssText = "padding-bottom: 170px;"+
+        " background: linear-gradient(180deg, rgb(65,62,62) 20%, rgb(65,62,62) 35%, rgb(174,198,144) 20%, rgb(174,198,144) 20%, rgb(174,198,144) 20%);"    
+        
+    } else {
+        board.style.cssText = "display:none";
+        but1.style.cssText = "visibility: visible;";
+        but2.style.cssText = "visibility: visible;";
+        but3.style.cssText = "visibility: visible;";
+        plusbox.style.cssText = "padding-bottom: 35px;"
+    }
+}
+function clickInput(value) {
+    if(input1 === true) {
+        document.getElementById("num1").value += value;
+    } else if (input2 === true) {
+        document.getElementById("num2").value += value;
+    }
+}
+function deleteOneElement() {
+    let tmp;
+    if(input1 === true) {
+        tmp = document.getElementById("num1").value;
+        tmp = tmp.slice(0,tmp.length - 1);
+        document.getElementById("num1").value  = tmp;
+        console.log(tmp);
+    } else if (input2 === true) {
+        tmp = document.getElementById("num2").value;
+        tmp = tmp.slice(0, tmp.length - 1);
+        document.getElementById("num2").value  = tmp;
+        console.log(tmp);
+    }
 }
